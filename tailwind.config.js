3c70b6cc24c096291118715f9f64dcb013da682d
Bryce Wray <bwray@wraytx.com>
2020-11-19 13:46:29 -0600

    Updating config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,32 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
     content: [
       './themes/hscg-starter/layouts/**/*.html',
     ],
   },
+  darkMode: 'media',
   theme: {
-    screens: {
-      'sm': '640px',  // => @media (min-width: 640px) { ... }
-      'md': '768px',  // => @media (min-width: 768px) { ... }
-      'lg': '1024px', // => @media (min-width: 1024px) { ... }
-      'xl': '1280px', // => @media (min-width: 1280px) { ... }
-    },
-    extend: {
-      screens: {
-        'dark': {
-          'raw': '(prefers-color-scheme: dark)',
-        },
-      },
-      colors: {
-        blue: {
-          '100': '#bbeeff',
-          '200': '#00aaff',
-          '300': '#0088ff',
-          '400': '#0033ff',
-          '500': '#0000ff',
-          '600': '#0000bb',
-          '700': '#0000aa',
-          '800': '#000088',
-          '900': '#000066',
-        },
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      black: '#000000',
+      gray: colors.gray,
+      yellow: colors.yellow,
+      blue: {
+        '100': '#bbeeff',
+        '200': '#00aaff',
+        '300': '#0088ff',
+        '400': '#0033ff',
+        '500': '#0000ff',
+        '600': '#0000bb',
+        '700': '#0000aa',
+        '800': '#000088',
+        '900': '#000066',
       },
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [], // if we add forms, do it here
 }
